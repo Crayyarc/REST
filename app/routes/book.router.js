86ae2cab04bf484177ab6b2e8d3a3cf7298188c0
Crayyarc <122812033+Crayyarc@users.noreply.github.com>
2023-03-22 14:53:40 +0000

    Add files via upload

---
+++ app/routes/book.router.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const {
+    listBooks,
+    createBook
+} = require('../controllers/book.controller')
+
+router.get('/', listBooks)
+
+router.post('/', createBook)
+
+module.exports = router
